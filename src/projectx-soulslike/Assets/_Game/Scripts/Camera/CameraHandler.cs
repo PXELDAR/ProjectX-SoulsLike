03b811a0587716acf98381b6ceecf02422bf130f
Xeldar <srd.al@iamgod.in>
2022-05-20 16:28:55 +0300

    Added camera collision

--- src/projectx-soulslike/Assets/_Game/Scripts/Camera/CameraHandler.cs
+++ src/projectx-soulslike/Assets/_Game/Scripts/Camera/CameraHandler.cs
@@ -16,15 +16,22 @@ namespace PXELDAR
         public float lookSpeed = 0.1f;
         public float followSpeed = 0.1f;
         public float pivotSpeed = 0.03f;
+        public float minimumPivot = -35;
+        public float maximumPivot = 35;
+        public float cameraSphereRadius = 0.2f;
+        public float cameraCollisionOffset = 0.2f;
+        public float minimumCollisionOffset = 0.2f;
 
         private Transform _myTransform;
-        private Vector3 _cameraTransformPosition;
         private LayerMask _ignoreLayers;
+        private Vector3 _cameraTransformPosition;
+        private Vector3 _cameraFollowVelocity;
+
+        private float _targetPosition;
         private float _defaultPosition;
         private float _lookAngle;
         private float _pivotAngle;
-        public float minimumPivot = -35;
-        public float maximumPivot = 35;
+
 
         //=================================================================================================
 
@@ -41,8 +48,13 @@ namespace PXELDAR
 
         public void FollowTarget(float delta)
         {
-            Vector3 targetPosition = Vector3.Lerp(transform.position, targetTransform.position, delta / followSpeed);
+            // Vector3 targetPosition = Vector3.Lerp(transform.position, targetTransform.position, delta / followSpeed);
+            Vector3 targetPosition =
+                Vector3.SmoothDamp(_myTransform.position, targetTransform.position, ref _cameraFollowVelocity, delta / followSpeed);
+
             _myTransform.position = targetPosition;
+
+            HandleCameraCollisions(delta);
         }
 
         //=================================================================================================
@@ -63,6 +75,30 @@ namespace PXELDAR
 
             targetRotation = Quaternion.Euler(rotation);
             cameraPivotTransform.localRotation = targetRotation;
+        }
+
+        //=================================================================================================
+
+        private void HandleCameraCollisions(float delta)
+        {
+            _targetPosition = _defaultPosition;
+            RaycastHit hit;
+            Vector3 direction = cameraTransform.position - cameraPivotTransform.position;
+            direction.Normalize();
+
+            if (Physics.SphereCast(cameraPivotTransform.position, cameraSphereRadius, direction, out hit, Mathf.Abs(_targetPosition), _ignoreLayers))
+            {
+                float distance = Vector3.Distance(cameraPivotTransform.position, hit.point);
+                _targetPosition = -(distance - cameraCollisionOffset);
+            }
+
+            if (Mathf.Abs(_targetPosition) < minimumCollisionOffset)
+            {
+                _targetPosition = -minimumCollisionOffset;
+            }
+
+            _cameraTransformPosition.z = Mathf.Lerp(cameraTransform.localPosition.z, _targetPosition, delta / 0.2f);
+            cameraTransform.localPosition = _cameraTransformPosition;
         }
 
         //=================================================================================================
